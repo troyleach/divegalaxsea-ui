ca904e86bc1e582b20c67204070649e6e3470327
RaulRoman <raulromanjr@gmail.com>
2020-02-22 13:37:36 -0500

    Fixed sizing of carousel in mobile size

--- src/components/PictureCarousel.js
+++ src/components/PictureCarousel.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
-import './PictureCarousel.css';
+import "./PictureCarousel.css";
 
-import Spinner from './Spinner';
-import homeImageGallery from '../data/homeImageGallery';
+import Spinner from "./Spinner";
+import homeImageGallery from "../data/homeImageGallery";
 
-const renderImages = (images) => {
-  return (
-    images.map((image, idx) => (
-      <>
-        <div key={idx}>
-          <img
-            className="picture"
-            src={image.url}
-            alt={image.alt}
-          />
-        </div>
-      </>
-    ))
-  )
-}
+const renderImages = images => {
+  return images.map((image, idx) => (
+    <>
+      <div key={idx}>
+        <img className="picture" src={image.url} alt={image.alt} />
+      </div>
+    </>
+  ));
+};
 
 // https://github.com/leandrowd/react-responsive-carousel
 class PictureCarousel extends Component {
@@ -29,20 +23,20 @@ class PictureCarousel extends Component {
     super(props);
     this.state = {
       images: null
-    }
+    };
   }
 
   async componentDidMount() {
     // Need to code in a way to pass the image lib I want
     const images = homeImageGallery();
     this.setState({
-      images: images,
-    })
+      images: images
+    });
   }
 
   render() {
-    const { images } = this.state
-    if (!images) return <Spinner />
+    const { images } = this.state;
+    if (!images) return <Spinner />;
     return (
       <Carousel
         autoPlay
@@ -51,14 +45,13 @@ class PictureCarousel extends Component {
         showStatus={false}
         showIndicators={false}
         showThumbs={false}
-        interval='2000'
-        width='600px'
-        className='carousel-container'
+        interval="2000"
+        width="auto"
+        className="carousel-container"
       >
         {renderImages(images)}
       </Carousel>
     );
-  };
-
-};
+  }
+}
 export default PictureCarousel;
